b9d3044442a728c0aafbe14084418e3ac457b240
Dem0nhunt3R <d3m0nhunt3r92@gmail.com>
2023-11-22 19:23:36 +0200

    main

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ let lastName = 'lastName';
 let person = firstName + ' ' + middleName + ' ' + lastName;
 
 //- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-let a = 100;
+let a = 1001;
 let b = '100';
 let c = true;
 console.log(typeof a);
